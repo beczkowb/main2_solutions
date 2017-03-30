9b68b3e2f8113e4d4f977a8d673d997fa48ce8c7
Bartłomiej Bęczkowski <beczkowb@gmail.com>
2017-03-30 14:54:10 +0200

    dluga_tasma

--- dluga_tasma.cpp
+++ dluga_tasma.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
 int main() {
-  cout << "ljlkj" << endl;  
+    int n;
+    int min_val;
+    int A[n];
+    int i;
+    cin >> n;
+
+    for (i=0; i < n; i++)
+        cin >> A[i];
+
+    min_val = A[0];
+    for (i=1; i < n; i++)
+        if (min_val > A[i])
+            min_val = A[i];
+
+    cout << min_val << ".00" << endl;
 }
